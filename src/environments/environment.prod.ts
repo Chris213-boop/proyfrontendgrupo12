df807a8d97ff1ac79b17f5e1ff2f0f71dce8abca
josuedurann <josueduran154@gmail.com>
2026-07-08 21:22:29 -0300

    environment wachineadas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjhhMDczYWM2ZTJhYzQwYTM5MWY5OWE2MWEyNzBmMDJiIiwiaCI6Im11cm11cjY0In0=',
-  emailjsServiceId: 'TU_EMAILJS_SERVICE_ID',
-  emailjsTemplateId: 'TU_EMAILJS_TEMPLATE_ID',
-  emailjsPublicKey: 'TU_EMAILJS_PUBLIC_KEY'
+  emailjsServiceId: 'service_rqzzf4c',
+  emailjsTemplateId: 'template_y54ccbr',
+  emailjsPublicKey: 'QYWqfUYkWNybnPL2G'
 };

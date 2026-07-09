8c3033ff2ab7dddac302ac7fdc08a9e90173c2c9
Torres Pablo <eneporene25@gmail.com>
2026-07-09 13:06:32 -0300

    Corrijo token admin en rutas usuario

--- src/app/services/usuario.ts
+++ src/app/services/usuario.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,76 +10,22 @@ export class UsuarioService {
 
   private urlBase = 'http://localhost:3000/api/usuario/';
 
-  obtenerUsuarios(): Observable<Usuario[]> {
-    return new Observable((observador) => {
-      fetch(this.urlBase)
-        .then(async (respuesta) => {
-          if (!respuesta.ok) {
-            throw new Error('No se pudieron cargar los usuarios');
-          }
+  constructor(private http: HttpClient) {}
 
-          const datos = await respuesta.json();
-          observador.next(datos);
-          observador.complete();
-        })
-        .catch((error) => observador.error(error));
-    });
+  obtenerUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.urlBase); 
   }
 
   agregarUsuario(usuario: Usuario): Observable<any> {
-    return new Observable((observador) => {
-      fetch(this.urlBase, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(usuario)
-      })
-        .then(async (respuesta) => {
-          const datos = await respuesta.json();
-          if (!respuesta.ok) {
-            throw new Error(datos.msg || 'No se pudo crear el usuario');
-          }
-
-          observador.next(datos);
-          observador.complete();
-        })
-        .catch((error) => observador.error(error));
-    });
+    return this.http.post<any>(this.urlBase, usuario);
   }
 
   actualizarUsuario(id: number, usuario: Usuario): Observable<any> {
-    return new Observable((observador) => {
-      fetch(`${this.urlBase}${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(usuario)
-      })
-        .then(async (respuesta) => {
-          const datos = await respuesta.json();
-          if (!respuesta.ok) {
-            throw new Error(datos.msg || 'No se pudo actualizar el usuario');
-          }
-
-          observador.next(datos);
-          observador.complete();
-        })
-        .catch((error) => observador.error(error));
-    });
+    return this.http.put<any>(`${this.urlBase}${id}`, usuario);
   }
 
   eliminarUsuario(id: number): Observable<any> {
-    return new Observable((observador) => {
-      fetch(`${this.urlBase}${id}`, { method: 'DELETE' })
-        .then(async (respuesta) => {
-          const datos = await respuesta.json();
-          if (!respuesta.ok) {
-            throw new Error(datos.msg || 'No se pudo eliminar el usuario');
-          }
-
-          observador.next(datos);
-          observador.complete();
-        })
-        .catch((error) => observador.error(error));
-    });
+    return this.http.delete<any>(`${this.urlBase}${id}`);
   }
 
 }

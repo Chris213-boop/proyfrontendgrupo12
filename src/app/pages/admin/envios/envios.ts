ed8e664314b19e54d0f75bd5a33bf3e233255143
Torres Pablo <eneporene25@gmail.com>
2026-07-10 00:07:59 -0300

    vista de la tabla pedidos

--- src/app/pages/admin/envios/envios.ts
+++ src/app/pages/admin/envios/envios.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pedido } from '../../../models/pedido';
+import { PedidoService } from '../../../services/pedido';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-envios',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './envios.html',
   styleUrl: './envios.css',
 })
-export class Envios {}
+export class Envios implements OnInit {
+
+  pedidos: Pedido[] = [];
+
+  constructor(private pedidoService: PedidoService) { }
+
+  ngOnInit(): void {
+    this.cargarPedidos();
+  }
+
+  cargarPedidos(): void {
+    this.pedidoService.obtenerPedidos().subscribe({
+      next: (data) => {
+        this.pedidos = data;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+}
